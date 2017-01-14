4c4092d7d70ebec8c7403fe75d8252035c4ce7c7
K Shah <kshah707@gmail.com>
2017-01-14 18:29:25 -0500

    Move Players Up/Down Working
    Small bug: often requires several mouse clicks before it works

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var playerList = [];
+var commentList = [];
+
 function addPlayer() {
 	var firstNameBox = document.getElementById("firstnamebox");
 	var lastNameBox = document.getElementById("lastnamebox");
@@ -5,58 +8,40 @@ function addPlayer() {
 	var lastName = lastNameBox.value;
 	
 	var fullName = firstName + " " + lastName;
-	var name = lastName.substring(0, 5).toLowerCase() + firstName.substring(0, 2).toLowerCase();
 	
-	var div_playerRank = document.getElementById("player_ranker");
-	var bioTemplate = document.getElementById("template_player-bio");
-	var newBio = bioTemplate.content.cloneNode(true);
-	
-	var namebox = newBio.getElementById("namebox");
-	var imgbox = newBio.getElementById("photo");
-	namebox.innerHTML = fullName;
-	imgbox.alt = fullName;
-	imgbox.src = imgbox.src + name + "01-2015.jpg";
-	
-	div_playerRank.appendChild(newBio);
-	
-	firstNameBox.value = "";
-}
-
-function addPlayer2() {
-	var firstNameBox = document.getElementById("firstnamebox");
-	var lastNameBox = document.getElementById("lastnamebox");
-	var firstName = firstNameBox.value;
-	var lastName = lastNameBox.value;
-	
-	var fullName = firstName + " " + lastName;
-	//the hockeyreference url name
-	var name = (lastName.substring(0, 5) + firstName.substring(0, 2)).toLowerCase();
-	
-	var div_playerRank = document.getElementById("player_ranker");
-	var bioTemplate = document.getElementById("template_player-bio");
-	var newBio = bioTemplate.content.cloneNode(true);
-	
-	var namebox = newBio.getElementById("namebox");
-	var imgbox = newBio.getElementById("photo");
-	namebox.innerHTML = fullName;
-	imgbox.alt = fullName;
-	imgbox.src = imgbox.src + name + "01-2015.jpg";
-	
-	if (playerList.indexOf(fullName) < 0) {
+	var index = playerList.indexOf(fullName);
+	if (index < 0) //doesn't exist in array
+	{
+		var URLName = lastName.substring(0, 5).toLowerCase() + firstName.substring(0, 2).toLowerCase();
+		
+		var div_playerRank = document.getElementById("player_ranker");
+		var bioTemplate = document.getElementById("template_player-bio");
+		var newBio = bioTemplate.content.cloneNode(true);
+		
+		var namebox = newBio.getElementById("namebox");
+		var imgbox = newBio.getElementById("photo");
+		namebox.innerHTML = fullName;
+		imgbox.alt = fullName;
+		imgbox.src = imgbox.src + URLName + "01-2015.jpg";
+		
+		div_playerRank.appendChild(newBio);
 		playerList.push(fullName);
-		scoreList.push(0);
 	}
-	div_playerRank.appendChild(newBio);
 	
 	firstNameBox.value = "";
 	lastNameBox.value = "";
 }
 
-/* function Upvote(elem) {
-	//parent is span, its parent is entire row, 
-	var playerName = elem.parentNode.parentNode.getElementById().innerHTML;
+function Upvote(elem) {
+	var thisRow = elem.parentNode.parentNode;
+	var container = thisRow.parentNode; //the container for all playerRow objects
+	//container.removeChild(thisRow);
+	container.insertBefore(thisRow, thisRow.previousSibling);
 }
 
 function Downvote(elem) {
-	
-} */
+	var thisRow = elem.parentNode.parentNode;
+	var container = thisRow.parentNode; //the container for all playerRow objects
+	//container.removeChild(thisRow);
+	container.insertBefore(thisRow, thisRow.nextSibling.nextSibling);
+}
