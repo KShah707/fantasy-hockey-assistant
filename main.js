a9be957d5cf19d59894646410e3ede7766a9757c
K Shah <kshah707@gmail.com>
2017-01-14 18:57:08 -0500

    Move Players Up/Down v1.1
    Removed internal list (to be implemented differently, later). Also added
    image buttons--need to be downloaded as assets later!

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var playerList = [];
-var commentList = [];
-
 function addPlayer() {
 	var firstNameBox = document.getElementById("firstnamebox");
 	var lastNameBox = document.getElementById("lastnamebox");
@@ -8,25 +5,19 @@ function addPlayer() {
 	var lastName = lastNameBox.value;
 	
 	var fullName = firstName + " " + lastName;
+	var URLName = lastName.substring(0, 5).toLowerCase() + firstName.substring(0, 2).toLowerCase();
 	
-	var index = playerList.indexOf(fullName);
-	if (index < 0) //doesn't exist in array
-	{
-		var URLName = lastName.substring(0, 5).toLowerCase() + firstName.substring(0, 2).toLowerCase();
-		
-		var div_playerRank = document.getElementById("player_ranker");
-		var bioTemplate = document.getElementById("template_player-bio");
-		var newBio = bioTemplate.content.cloneNode(true);
-		
-		var namebox = newBio.getElementById("namebox");
-		var imgbox = newBio.getElementById("photo");
-		namebox.innerHTML = fullName;
-		imgbox.alt = fullName;
-		imgbox.src = imgbox.src + URLName + "01-2015.jpg";
-		
-		div_playerRank.appendChild(newBio);
-		playerList.push(fullName);
-	}
+	var div_playerRank = document.getElementById("player_ranker");
+	var bioTemplate = document.getElementById("template_player-bio");
+	var newBio = bioTemplate.content.cloneNode(true);
+	
+	var namebox = newBio.getElementById("namebox");
+	var imgbox = newBio.getElementById("photo");
+	namebox.innerHTML = fullName;
+	imgbox.alt = fullName;
+	imgbox.src = imgbox.src + URLName + "01-2015.jpg";
+	
+	div_playerRank.appendChild(newBio);
 	
 	firstNameBox.value = "";
 	lastNameBox.value = "";
@@ -37,6 +28,8 @@ function Upvote(elem) {
 	var container = thisRow.parentNode; //the container for all playerRow objects
 	//container.removeChild(thisRow);
 	container.insertBefore(thisRow, thisRow.previousSibling);
+	
+	
 }
 
 function Downvote(elem) {
